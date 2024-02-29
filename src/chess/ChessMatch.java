3badf6625012ad6e2be02846355f52d441fe45d4
camarfbi <camarfbi@hotmail.com>
2024-02-29 08:48:28 -0300

    Litle improviment in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,16 +23,16 @@ public class ChessMatch { //classe com as regras do jogo de xadrez; Quem tem que
 		return mat;
 	}
 	
-	//metodo recebe cordenadas do xadrez
+	//metodo recebe coordenadas do xadrez
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
 	private void initialSetup(){
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));//passando as posicoes do xadrez
+		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
